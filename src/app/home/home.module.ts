9c4e1efa8f01b51e5b21f31ed50ac3e9235ced8a
Modesto <msmd@gft.com>
2019-05-07 16:17:48 -0300

    Componentizando mensagens de validação

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { VmessageComponent } from '../shared/components/vmessage/vmessage.component';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [SigninComponent],
-    imports: [CommonModule, ReactiveFormsModule]
+    imports: [CommonModule, ReactiveFormsModule, VmessageModule]
 })
 export class HomeModule {
 
---
+++ src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VmessageComponent } from './vmessage.component';
+
+@NgModule({
+    declarations:[VmessageComponent],
+    exports: [VmessageComponent]
+})
+export class VmessageModule {
+
+}

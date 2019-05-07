265df7eded506345ca96fc243b34c52052f33106
Modesto <msmd@gft.com>
2019-05-07 16:18:21 -0300

    Componentizando mensagens de validação

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { VmessageComponent } from '../shared/components/vmessage/vmessage.component';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
